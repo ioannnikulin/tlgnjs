2a21ff30b8378d8fc052f29c3905fa4874e36b55
ivnik <ioann.nikulin@gmail.com>
2018-10-24 14:59:54 +0300

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const TelegramBot=require('node-telegram-bot-api')
+const TOKEN='697600599:AAET3B6vHbp4BPeXGwPbhgC5YlZYyYC311U'
+const bot=TelegramBot(TOKEN, {polling: true})
+bot.on('message', msg => {
+bot.sendMessage(msg.chat.id, 'Hello, ${msg.from.first_name}')
+})
