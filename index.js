c21138df268306354611f4ef731dd62239ba13cd
ivnik <ioann.nikulin@gmail.com>
2018-10-24 15:33:56 +0300

    5

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const TOKEN = process.env.TLGTOKEN
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
+bot.onText(/\/test/, (msg, match) => {
+	bot.sendMessage(msg.chat.id, `test success`)
+})
+
 bot.on('message', msg => {
   bot.sendMessage(msg.chat.id, `Hello, ${msg.from.first_name}`)
 })
